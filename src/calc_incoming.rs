bcad2a8204ec5136f8db1b5458a7f4b448961854
Daegonica <memphis1988s@gmail.com>
2025-12-12 14:53:19 -0700

    Can now search within a date range.

--- src/calc_incoming.rs
+++ src/calc_incoming.rs
@@ -3,23 +3,15 @@ use crate::prelude::*;
 pub fn calc_amount_earned(log: &mut Logger, records: &[Record], options: &Vec<String>) {
     log.info("Calculating totals!");
 
-    let date_range_re = Regex::new(r"^(\d{4}-\d{2}-\d{2}) (\d{4}-\d{2}-\d{2})$").unwrap();
+    let mut default_search: bool = true;
     let mut start_date: Option<NaiveDate> = None;
     let mut end_date: Option<NaiveDate> = None;
+    let mut record_date: Option<NaiveDate> = None;
 
     if let Some(first) = options.get(0) {
         if first != "complete" {
-            match first {
-                opt if date_range_re.is_match(opt) => {
-                    let caps = date_range_re.captures(opt).unwrap();
-                    start_date = NaiveDate::parse_from_str(&caps[1], "%Y-%m-%d").ok();
-                    end_date = NaiveDate::parse_from_str(&caps[2], "%Y-%m-%d").ok();
-                    log.info("Setting date range.");
-                }
-                _ => {
-                    log.info("Invalid date range format. Ignoring filters. Going for complete data.");
-                }
-            };
+            (start_date, end_date) = set_date_range(first, log);
+            default_search = false;
         }
     }
 
@@ -27,8 +19,17 @@ pub fn calc_amount_earned(log: &mut Logger, records: &[Record], options: &Vec<St
     let ignore_info = Regex::new(r"Internet Banking INTERNET TRANSFER \d+").unwrap();
 
     for record in records {
-
-        if !ignore_info.is_match(&record.info) {
+        let mut record_info: bool = true;
+        
+        if !default_search {
+            // Need to add logic to handle date range, and optional categories.
+            record_date = NaiveDate::parse_from_str(&record.date, "%Y-%m-%d").ok();
+            if !(record_date >= start_date && record_date <= end_date) {
+                record_info = false;
+            }
+        }
+                
+        if !ignore_info.is_match(&record.info) && record_info {
             total_earned += match record.money_in.parse::<f64>() {
                 Ok(value) => {
                     log.info(format!("{}", record.date));
@@ -36,7 +37,31 @@ pub fn calc_amount_earned(log: &mut Logger, records: &[Record], options: &Vec<St
                 },
                 Err(_) => 0.0,
             };
+        
         }
+
     }
     log.info(format!("Total Earned: {}", (total_earned *  100.0).round() / 100.0));
 }
+
+fn set_date_range(range: &String, log: &mut Logger) -> (Option<NaiveDate>, Option<NaiveDate>) {
+
+    let date_range_re = Regex::new(r"^(\d{4}-\d{2}-\d{2}) (\d{4}-\d{2}-\d{2})$").unwrap();
+    let mut start_date: Option<NaiveDate> = None;
+    let mut end_date: Option<NaiveDate> = None;
+
+    match range {
+        opt if date_range_re.is_match(opt) => {
+            let caps = date_range_re.captures(opt).unwrap();
+            start_date = NaiveDate::parse_from_str(&caps[1], "%Y-%m-%d").ok();
+            end_date = NaiveDate::parse_from_str(&caps[2], "%Y-%m-%d").ok();
+            log.info("Setting date range.");
+        }
+         _ => {
+            log.info("Invalid date range format. Ignoring filters. Going for complete data.");
+        }
+    };
+
+    (start_date, end_date)
+}
+    
