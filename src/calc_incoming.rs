243ba59e3ea90b279a69442bf868bcbfe94ae52e
Daegonica <memphis1988s@gmail.com>
2025-12-13 05:01:15 -0700

    Added once_cell for lazy static variables across files.

--- src/calc_incoming.rs
+++ src/calc_incoming.rs
@@ -9,7 +9,7 @@ pub fn calc_amount_earned(log: &mut Logger, records: &[Record], options: &Vec<St
     let mut record_date: Option<NaiveDate> = None;
 
     if let Some(first) = options.get(0) {
-        if first != "complete" {
+        if DATE_FORMAT.is_match(&first) {
             (start_date, end_date) = set_date_range(first, log);
             default_search = false;
         }
@@ -32,27 +32,25 @@ pub fn calc_amount_earned(log: &mut Logger, records: &[Record], options: &Vec<St
         if !ignore_info.is_match(&record.info) && record_info {
             total_earned += match record.money_in.parse::<f64>() {
                 Ok(value) => {
-                    log.info(format!("{}", record.date));
                     (value * 100.00).round() / 100.0
                 },
                 Err(_) => 0.0,
             };
         
         }
-
     }
+
     log.info(format!("Total Earned: {}", (total_earned *  100.0).round() / 100.0));
 }
 
 fn set_date_range(range: &String, log: &mut Logger) -> (Option<NaiveDate>, Option<NaiveDate>) {
 
-    let date_range_re = Regex::new(r"^(\d{4}-\d{2}-\d{2}) (\d{4}-\d{2}-\d{2})$").unwrap();
     let mut start_date: Option<NaiveDate> = None;
     let mut end_date: Option<NaiveDate> = None;
 
     match range {
-        opt if date_range_re.is_match(opt) => {
-            let caps = date_range_re.captures(opt).unwrap();
+        opt if DATE_FORMAT.is_match(opt) => {
+            let caps = DATE_FORMAT.captures(opt).unwrap();
             start_date = NaiveDate::parse_from_str(&caps[1], "%Y-%m-%d").ok();
             end_date = NaiveDate::parse_from_str(&caps[2], "%Y-%m-%d").ok();
             log.info("Setting date range.");
--- src/prelude.rs
+++ src/prelude.rs
@@ -2,3 +2,6 @@ pub use regex::Regex;
 pub use chrono::NaiveDate;
 pub use dlog::{*, enums::OutputTarget};
 pub use crate::Record;
+use once_cell::sync::Lazy;
+
+pub static DATE_FORMAT: Lazy<Regex> = Lazy::new(|| Regex::new(r"^(\d{4}-\d{2}-\d{2}) (\d{4}-\d{2}-\d{2})$").unwrap());
