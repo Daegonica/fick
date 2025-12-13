407039ac692836ad5f252e3bfcae731679eba9ef
Daegonica <memphis1988s@gmail.com>
2025-12-13 12:26:59 -0700

    Added the ability to show what filter patterns are't currently in the config.toml file.

--- src/lib.rs
+++ src/lib.rs
@@ -67,58 +67,60 @@ impl FickCLI {
         self.log.info("All info recorded!");
 
         let _ = self.load_filters();
-        let _ = self.compare_filters_to_data();
 
         let mut default_option: Vec<String> = Vec::new();
-        if options.is_empty() {
-            default_option.push("complete".to_string());
-        } else {
-            default_option = options.clone()
+        match options.as_slice() {
+            [] => {
+                default_option.push("complete".to_string())
+            }
+            _ => { 
+                default_option = options.clone();
+            }
         }
 
 
         // TODO: Add in functions of what we want to do. By date, by transaction, by amount
         match query.as_str() {
             "total earned" => calc_incoming::calc_amount_earned(&mut self.log, &self.records, &default_option, &self.filters),
+            "filters needed" => self.show_filters_to_add(),
             _ => self.log.info("Didn't pick a query!"),
         }
 
         Ok(())
     }
 
-    fn compare_filters_to_data(&mut self) {
+    pub fn convert_to_regex(&mut self) -> Vec<Regex> {
+
+        let mut regex_patterns: Vec<Regex> = Vec::new();
+
+        for filter in &self.filters.filters {
+            for pattern in &filter.patterns {
+                let lower_pattern = pattern.to_lowercase();
+                if let Ok(regex) = Regex::new(&lower_pattern) {
+                    regex_patterns.push(regex);
+                    // self.log.info("Found a Regex to match!");
+                }
+            }
+        }
+
+        regex_patterns
+    }
+
+    fn show_filters_to_add(&mut self) {
 
         let mut records: Vec<String> = Vec::new();
         let re = Regex::new(r"\d+").unwrap();
 
+        let regex_patterns = self.convert_to_regex();
+
         for record in self.records.clone() {
-
-            // Need to compare the config.toml patterns with each record.info string before normalizing.
-            // We only want to normalize and save the info if we don't currently have a pattern for it.
-            for filter in &self.filters.filters {
-                // Each 'filter' has a set of patterns.
-                    // Each set of patterns need to be checked against every 'record'
-                        // 'patterns' are in String format.
-                        // Need to convert to regex or compare against String if not.
-                    // 'record' holds the 'info', 'money_in', 'money_out'
-                    // If any of the patterns in the 'filter' matches the 'info' we want to ignore it
-                    // If none of the patterns match the 'info' then we want to mark it storage
-                let mut string_patterns: Vec<String> = Vec::new();
-                let mut regex_patterns: Vec<Regex> = Vec::new();
+            let info_lower = record.info.to_lowercase();
+            if !regex_patterns.iter().any(|re| re.is_match(&info_lower)) {
                 
-                for pattern in &filter.patterns {
-                    if let Ok(regex) = Regex::new(pattern) {
-                        regex_filters.push(regex);
-                    } else {
-                        string_filters.push(pattern.clone());
-                    }
+                let normalized = re.replace_all(&record.info, "").to_string();
+                if !records.contains(&normalized) {
+                    records.push(normalized);
                 }
-            }
-
-            // Normalizes all info lines and places into a Vector ensuring there are no doubles.
-            let normalized = re.replace_all(&record.info, "").to_string();
-            if !records.contains(&normalized) {
-                records.push(normalized);
             }
 
         }
